580e653f11b80301c4546e9bfa76f4dea1382e5a
Jesse Hoyos <jesse@mojotech.com>
2020-08-24 12:33:09 -0400

    initialize

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import path from 'path';
+import { mkdir, rm } from 'shelljs';
+
+const OUT_DIR = 'out';
+const cwd = process.cwd();
+
+export default async (cmd: string) => {
+  if (cmd === 'build') {
+    console.log('building...');
+    mkdir('-p', path.join(cwd, OUT_DIR));
+  }
+  if (cmd === 'pack') {
+    console.log('packing...');
+  }
+  if (cmd === 'watch') {
+    console.log('watching...');
+  }
+  if (cmd === 'clean') {
+    console.log('cleaning...');
+    rm('-rf', OUT_DIR);
+  }
+  if (cmd === 'eject') {
+    console.log('ejecting...');
+  }
+
+  return console.log(cmd);
+};
