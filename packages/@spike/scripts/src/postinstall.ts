0fe781d940e9ab3a5da94a410ed1666ac5fc068f
Chris D. MacRae <hello@chrisdmacrae.com>
2020-09-06 15:32:58 -0300

    Add postinstall instructions for scripts

---
+++ packages/@spike/scripts/src/postinstall.ts
@@ -0,0 +1,35 @@
+import { existsSync, readFileSync } from 'fs';
+import { join } from 'path';
+
+const PKG_PATH = join(process.cwd(), "package.json");
+
+if (existsSync(PKG_PATH)) {
+  const pkg = JSON.parse(readFileSync(PKG_PATH, { encoding: 'utf-8' }));
+
+  if (pkg['name'] !== "@spike/scripts") {
+    console.log(`
+Thanks for installing @spike/scripts!
+---
+
+This is a utility for building web modules for spike,
+without worrying about maintaining a complicated build
+and test process. spike-scripts allows you to build
+spike-ready modules that will build (almost) forever.
+
+To complete setup, please add these package.json scripts:
+
+{
+  "build": "spike-scripts build",
+  "develop: "spike-scripts develop --watch",
+  "test": "spike-scripts test",
+  "test-watch": "spike-scripts test --watch"
+}
+
+Also, add a jest.config.js to the root with:
+
+const { createTestingConfig } = require('@spike/scripts');
+
+module.exports = createTestingConfig();
+    `);
+  }
+}
