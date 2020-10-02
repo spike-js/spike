be627acf327926063abcb75f2a43042533435124
Jesse Hoyos <jesse@mojotech.com>
2020-10-02 14:54:07 -0400

    feature(parser): make ids relative to  spikedir

--- packages/@spike/parser/src/nodes/base.ts
+++ packages/@spike/parser/src/nodes/base.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import findUp from 'find-up';
 import { getMime } from '../utils';
 
 export interface Node {
@@ -30,11 +31,34 @@ export type InternalPath = {
   };
 };
 
+export type SpikeConfigFile = 'spike.config.js' | '.spikerc';
+
+export function relativePathFromSpikeRoot(filepath: string): string {
+  const rootIdentifiers: SpikeConfigFile[] = ['spike.config.js', '.spikerc'];
+  const spikeRootDir = () => {
+    const config_filepath = findUp.sync(rootIdentifiers);
+    return (
+      config_filepath &&
+      path
+        .dirname(config_filepath)
+        .split('/')
+        .slice(-1)[0]
+    );
+  };
+
+  const root_dir = spikeRootDir();
+  if (root_dir) {
+    return filepath.split(root_dir)[1];
+  }
+  return filepath;
+}
+
 export async function createGraphNode(file: string): Promise<Node> {
+  const relativeUrl = relativePathFromSpikeRoot(path.resolve(file));
   return {
-    id: path.resolve(file),
+    id: relativeUrl,
     type: 'external',
-    nodeLocation: path.resolve(file),
+    nodeLocation: relativeUrl,
     mimeType: getMime(file),
     children: [],
   };
